3f082b27724c90e8ff2f44bd62b939aef508392e
Jedidiah Abera <yidididabera87@gmail.com>
2025-01-01 00:19:23 +0300

    add footer to the AppRouter

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -9,6 +9,9 @@ import PostDetail from "../pages/PostDetail";
 import PostEditor from "../pages/PostEditor";
 import SignupPage from "../pages/SignupPage";
 import Header from "../components/Header";
+import Footer from "../components/Footer"
+//import { Footer } from "flowbite-react";
+
 
 const AppRouter = () => {
   return (
@@ -24,6 +27,7 @@ const AppRouter = () => {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 };
