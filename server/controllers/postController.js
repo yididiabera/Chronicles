8aed6d07c8fb9189462f1f1b51c1bb275499f656
Jedidiah Abera <yidididabera87@gmail.com>
2025-01-03 17:31:37 +0300

    implement getPosts, getPostById, updatePostById, deleteById functions with error handling

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -23,3 +23,65 @@ export const createPost = async(req, res, next) => {
         next(error)
     }
 }
+
+export const getPosts = async(req, res, next) => {
+    try {
+     const posts = await Post.find().populate("author", "username email")   
+     res.status(200).json(posts)
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const getPostById = async(req, res, next) => {
+    try {
+        const {id} = req.params;
+
+        const post = await Post.findById(id).populate('author', "username email") 
+        if (!post) {
+            return res.status(404).json({message: "Post Not Found!"})
+        }
+        res.status(200).json(post)
+    } catch(error) {
+        next(error)
+    }
+
+}
+
+export const updatePostById = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id, 
+            {$set: req.body},
+            {new: true}
+        )
+        
+        if(!updatedPost) {
+            return res.status(404).json({message: "Page Not Found!"})
+        }
+
+        res.status(200).json(updatedPost)
+    }
+    catch(error) {
+        next(error);
+    }
+}
+
+const deleteById = async(req, res, next) {
+    try {
+        const {id} = req.params;
+
+        const deletedPost = await Post.findByIdAndDelete(id);
+
+        if(!deletedPost) {
+            return res.status(404).json({message: "Post not found"});
+        }
+        res.status(200).json({message :"Post deleted successfully."}) 
+    }
+    catch(error) [
+        next(error)
+    ]
+}
