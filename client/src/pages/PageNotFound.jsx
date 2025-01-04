f41f05c9f233f35b55ffd15fa5bd99a09fb12797
Jedidiah Abera <yididiabera87@gmail.com>
2025-01-05 00:57:30 +0300

    update: improve styling for the pageNotFound page

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -3,17 +3,15 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-yellow-100 text-gray-800">
-      <div className="bg-yellow-500 text-white p-4 rounded-md shadow-lg">
-        <h1 className="text-6xl font-bold mb-4">404</h1>
-        <p className="text-xl mb-4">Warning! The page you're looking for doesn't exist.</p>
-        <Link
-          to="/"
-          className="px-4 py-2 bg-red-600 text-white rounded-md shadow-lg hover:bg-red-700 transition duration-300"
-        >
-          Go Back Home
-        </Link>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-indigo-50 to-purple-200 dark:from-gray-800 dark:to-gray-900 text-gray-700 dark:text-gray-300">
+      <h1 className="text-6xl font-bold text-purple-600 dark:text-teal-400 mb-4">404</h1>
+      <p className="text-xl mb-8">Oops! The page you're looking for doesn't exist.</p>
+      <Link
+        to="/"
+        className="px-4 py-2 bg-purple-600 text-white rounded-md shadow-lg hover:bg-purple-700 transition duration-300"
+      >
+        Go Back Home
+      </Link>
     </div>
   );
 };
