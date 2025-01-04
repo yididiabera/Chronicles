d679f3d0e3044b7078a3b51de0aed3e2c1736b3c
Jedidiah Abera <yididiabera87@gmail.com>
2025-01-05 00:21:33 +0300

    implement a pagenotfound page

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function PageNotFound() {
-  return <div>PageNotFound</div>;
-}
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-yellow-100 text-gray-800">
+      <div className="bg-yellow-500 text-white p-4 rounded-md shadow-lg">
+        <h1 className="text-6xl font-bold mb-4">404</h1>
+        <p className="text-xl mb-4">Warning! The page you're looking for doesn't exist.</p>
+        <Link
+          to="/"
+          className="px-4 py-2 bg-red-600 text-white rounded-md shadow-lg hover:bg-red-700 transition duration-300"
+        >
+          Go Back Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
