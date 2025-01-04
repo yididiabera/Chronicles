2899f3286a1c3ec27fa91f05777128d602270d4d
Jedidiah Abera <yididiabera87@gmail.com>
2025-01-04 20:29:17 +0300

    fix cookies error

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,21 +1,32 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
+  console.log(req.cookies); // Log the cookies to check if access_token is present
 
-    if(!token) {
-        const error = new Error("Unauthorized.")
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    console.error("Access token is missing.");
+    const error = new Error("Unauthorized.");
+    error.status = 401;
+    return next(error);
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        console.error("Token expired:", err);
+        const error = new Error("Token expired. Please log in again.");
         error.status = 401;
         return next(error);
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, ( err, user
-    ) =>{ if(err) {
+      } else {
+        console.error("Invalid token:", err);
         const error = new Error("Invalid token.");
         error.status = 403;
         return next(error);
+      }
     }
     req.user = user;
-    next();});
-
-}
+    next();
+  });
+};
