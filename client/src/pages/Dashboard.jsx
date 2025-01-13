fd59617dfa74ddb4242ada4857d6475de5b4f014
Jedidiah Abera <yididiabera87@gmail.com>
2025-01-13 23:59:02 +0300

    update styling for the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,6 @@ import CreatePostForm from "../components/CreatePostForm";
 export default function Dashboard() {
     return (
         <div>
-            <h1>Dashboard</h1>
             <CreatePostForm />
         </div>
     )
