25d5813701c18f9f44579fd2fe1490d70856c4d8
Jedidiah Abera <yididiabera87@gmail.com>
2025-07-28 17:25:29 +0300

    improved the database setup

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
-  try {
-    const con = await mongoose.connect(process.env.MONGO_URI , { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-        serverSelectionTimeoutMS: 30000, });
-    console.log(`MongoDB connected: ${con.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+    try {
+        const con = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB connected: ${con.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
